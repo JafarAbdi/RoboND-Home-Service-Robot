45db322921aca212d9bb8ba0ddaef1de3c2a0bb9
JafarAbdi <cafer.abdi@gmail.com>
2019-01-19 13:25:46 +0300

    Finished testing slam task

--- ShellScripts/testing_slam.sh
+++ ShellScripts/testing_slam.sh
@@ -2,3 +2,12 @@
 #sleep 5
 xterm -e "roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/jafar/catkin_ws/src/RoboND-Home-Service-Robot/World/MyWorld.world" &
 sleep 5
+
+xterm -e "roslaunch turtlebot_gazebo gmapping_demo.launch" &
+sleep 5
+
+xterm -e "roslaunch turtlebot_rviz_launchers view_navigation.launch" &
+sleep 5
+
+xterm -e "roslaunch turtlebot_teleop keyboard_teleop.launch"
+
