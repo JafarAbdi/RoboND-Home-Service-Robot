367c0b31227b00b06770112b3724c2632ed68eaf
JafarAbdi <cafer.abdi@gmail.com>
2019-01-21 16:29:15 +0300

    Finish pick_objects.sh script

---
+++ ShellScripts/pick_objects.sh
@@ -0,0 +1,12 @@
+#xterm -e "roscore" &
+#sleep 5
+xterm -e "roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/jafar/catkin_ws/src/RoboND-Home-Service-Robot/World/MyWorld.world" &
+sleep 3
+
+xterm -e "roslaunch turtlebot_gazebo amcl_demo.launch map_file:=/home/jafar/catkin_ws/src/RoboND-Home-Service-Robot/Map/map.yaml" &
+sleep 3
+
+xterm -e "roslaunch turtlebot_rviz_launchers view_navigation.launch" &
+sleep 3
+
+xterm -e "rosrun pick_objects pick_objects_node"
